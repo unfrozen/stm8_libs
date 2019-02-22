0204ee059c3559247647fac6e5e729350239669d
Richard Hodges <richard@hodges.org>
2019-02-22 12:17:51 -0700

    Adding header files for _003 series to standard header.

--- stm8s_header.h
+++ stm8s_header.h
@@ -1,7 +1,12 @@
 /*
- * Choose stm8s_103 or stm8s_105 header,
+ * Choose stm8s_003, stm8s_103 or stm8s_105 header,
  * depending on compile directive.
  */
+
+#ifdef STM8003
+#include "stm8s_003.h"
+#include "stm8s_003.inc"
+#endif
 
 #ifdef STM8103
 #include "stm8_103.h"
