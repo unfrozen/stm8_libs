e75d34f59648584e57132bc549124513f0868b44
jackkum <jackkum@bk.ru>
2019-08-02 20:17:01 +0800

    Equalized indents

--- lib_ds3231.c
+++ lib_ds3231.c
@@ -9,11 +9,11 @@ uint8_t deviceAddress = 0;
  * Initialize the device
  */
 void ds3231_init(uint8_t address) {
-	if( ! deviceAddress){
-		i2c_init();
-	}
+  if( ! deviceAddress){
+    i2c_init();
+  }
 
-	deviceAddress = address;
+  deviceAddress = address;
 }
 
 uint8_t decToBcd(uint8_t val) {
@@ -25,10 +25,10 @@ uint8_t bcdToDec(uint8_t val) {
 }
 
 uint8_t startWrite(uint8_t address) {
-	uint8_t wa = TW_WRITE | (deviceAddress << 1);
-	uint8_t ra = TW_READ | wa;
+  uint8_t wa = TW_WRITE | (deviceAddress << 1);
+  uint8_t ra = TW_READ | wa;
 
-	i2c_start();
+  i2c_start();
 
   i2c_txbit8(wa);
   if ( ! i2c_getack()) {
@@ -44,10 +44,10 @@ uint8_t startWrite(uint8_t address) {
 }
 
 uint8_t startRead(uint8_t address) {
-	uint8_t wa = TW_WRITE | (deviceAddress << 1);
-	uint8_t ra = TW_READ | wa;
+  uint8_t wa = TW_WRITE | (deviceAddress << 1);
+  uint8_t ra = TW_READ | wa;
 
-	i2c_start();
+  i2c_start();
 
   i2c_txbit8(wa);
   if ( ! i2c_getack()) {
@@ -73,36 +73,36 @@ uint8_t startRead(uint8_t address) {
 }
 
 void stop(void) {
-	i2c_stop();
+  i2c_stop();
 }
 
 /*
  *  Get current datetime value
  */
 void ds3231_getTime(DATETIME *time) {
-	
-	if(startRead(REG_SEC)) {
-		time->second = bcdToDec(i2c_rxbit8() & 0x7f);
-	  i2c_sendack();
+  
+  if(startRead(REG_SEC)) {
+    time->second = bcdToDec(i2c_rxbit8() & 0x7f);
+    i2c_sendack();
 
-	  time->minute     = bcdToDec(i2c_rxbit8());
-	  i2c_sendack();
+    time->minute     = bcdToDec(i2c_rxbit8());
+    i2c_sendack();
 
-	  time->hour       = bcdToDec(i2c_rxbit8() & 0x3f); 
-	  i2c_sendack();
+    time->hour       = bcdToDec(i2c_rxbit8() & 0x3f); 
+    i2c_sendack();
 
-	  time->dayOfWeek  = bcdToDec(i2c_rxbit8());
-	  i2c_sendack();
+    time->dayOfWeek  = bcdToDec(i2c_rxbit8());
+    i2c_sendack();
 
-	  time->dayOfMonth = bcdToDec(i2c_rxbit8());
-	  i2c_sendack();
+    time->dayOfMonth = bcdToDec(i2c_rxbit8());
+    i2c_sendack();
 
-	  time->month      = bcdToDec(i2c_rxbit8());
-	  i2c_sendack();
+    time->month      = bcdToDec(i2c_rxbit8());
+    i2c_sendack();
 
-	  time->year       = bcdToDec(i2c_rxbit8());
-	  i2c_sendnak();
-	}
+    time->year       = bcdToDec(i2c_rxbit8());
+    i2c_sendnak();
+  }
 
   stop();
 }
@@ -112,28 +112,28 @@ void ds3231_getTime(DATETIME *time) {
  *  Set new datetime value
  */
 void ds3231_setTime(DATETIME *time) {
-	if(startWrite(REG_SEC)) {
-		i2c_txbit8(decToBcd(time->second));
-		i2c_getack();
+  if(startWrite(REG_SEC)) {
+    i2c_txbit8(decToBcd(time->second));
+    i2c_getack();
 
-		i2c_txbit8(decToBcd(time->minute));
-		i2c_getack();
+    i2c_txbit8(decToBcd(time->minute));
+    i2c_getack();
 
-		i2c_txbit8(decToBcd(time->hour));
-		i2c_getack();
+    i2c_txbit8(decToBcd(time->hour));
+    i2c_getack();
 
-		i2c_txbit8(decToBcd(time->dayOfWeek));
-		i2c_getack();
+    i2c_txbit8(decToBcd(time->dayOfWeek));
+    i2c_getack();
 
-		i2c_txbit8(decToBcd(time->dayOfMonth));
-		i2c_getack();
+    i2c_txbit8(decToBcd(time->dayOfMonth));
+    i2c_getack();
 
-		i2c_txbit8(decToBcd(time->month));
-		i2c_getack();
+    i2c_txbit8(decToBcd(time->month));
+    i2c_getack();
 
-		i2c_txbit8(decToBcd(time->year));
-		i2c_getack();
-	}
+    i2c_txbit8(decToBcd(time->year));
+    i2c_getack();
+  }
 
   stop();
 }
@@ -142,16 +142,16 @@ void ds3231_setTime(DATETIME *time) {
  *  Read temperature from module
  */
 float ds3231_getTemp(void) {
-	uint8_t _msb = 0, _lsb = 0;
+  uint8_t _msb = 0, _lsb = 0;
 
-	if(startRead(REG_TEMPM)) {
-		_msb = i2c_rxbit8();
-		i2c_sendack();
+  if(startRead(REG_TEMPM)) {
+    _msb = i2c_rxbit8();
+    i2c_sendack();
 
-		_lsb = i2c_rxbit8();
-		i2c_sendnak();
-	}
+    _lsb = i2c_rxbit8();
+    i2c_sendnak();
+  }
 
-	stop();
-	return (float)_msb + ((_lsb >> 6) * 0.25f);
+  stop();
+  return (float)_msb + ((_lsb >> 6) * 0.25f);
 }
--- lib_ds3231.h
+++ lib_ds3231.h
@@ -17,33 +17,33 @@
 
 #include <stdint.h>
 
-#define 	TW_READ   1
-#define 	TW_WRITE   0
+#define   TW_READ   1
+#define   TW_WRITE   0
 
-#define 	TRUE   1
-#define 	FALSE   0
+#define   TRUE   1
+#define   FALSE   0
 
-#define REG_SEC		0x00
-#define REG_MIN		0x01
-#define REG_HOUR	0x02
-#define REG_DOW		0x03
-#define REG_DATE	0x04
-#define REG_MON		0x05
-#define REG_YEAR	0x06
-#define REG_CON		0x0e
-#define REG_STATUS	0x0f
-#define REG_AGING	0x10
-#define REG_TEMPM	0x11
-#define REG_TEMPL	0x12
+#define REG_SEC   0x00
+#define REG_MIN   0x01
+#define REG_HOUR  0x02
+#define REG_DOW   0x03
+#define REG_DATE  0x04
+#define REG_MON   0x05
+#define REG_YEAR  0x06
+#define REG_CON   0x0e
+#define REG_STATUS  0x0f
+#define REG_AGING 0x10
+#define REG_TEMPM 0x11
+#define REG_TEMPL 0x12
 
 typedef struct {
-	uint8_t second;
-	uint8_t minute;
-	uint8_t hour;
-	uint8_t dayOfWeek;
-	uint8_t dayOfMonth;
-	uint8_t month;
-	uint8_t year;
+  uint8_t second;
+  uint8_t minute;
+  uint8_t hour;
+  uint8_t dayOfWeek;
+  uint8_t dayOfMonth;
+  uint8_t month;
+  uint8_t year;
 } DATETIME;
 
 /*
